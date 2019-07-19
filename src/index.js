bab90d4cfa5ec2e69073d62ffe142145ba4c4281
Dave <dslusser13@gmail.com>
2019-07-19 10:19:46 -0700

    added fitbit api call

--- src/index.js
+++ src/index.js
@@ -16,7 +16,46 @@ class App extends Component {
     }
 
     componentDidMount() {
-        //API to fitbit will go here in production
+        var fitbitAccessToken;
+
+        if (!window.location.hash) {
+            window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DFML&redirect_uri=https%3A%2F%2Fwww.dslusser.com%2FEIT.html&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
+        } else {
+            var fragmentQueryParameters = {};
+            window.location.hash.slice(1).replace(
+                new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+                function($0, $1, $2, $3) {
+                fragmentQueryParameters[$1] = $3;
+                }
+            );
+            fitbitAccessToken = fragmentQueryParameters.access_token;
+        }
+
+        async function fetchValues() {
+            var url = 'https://api.fitbit.com/1/user/-/activities/steps/date/today/1m.json';
+            var bearer = 'Bearer ' + fitbitAccessToken;
+            const response = fetch(url, {
+                method: 'GET',
+                'Access-Control-Allow-Credentials': true,
+                headers: {
+                    'Authorization': bearer,
+                    'Content-Type': 'application/json'
+                }
+                })
+                .then(response => response.json())
+                .then(json => {
+                let stepLog = json['activities-steps'];
+                let values = []; 
+                let dates = [];
+                for (let i = 30; i >= 24; i--) {
+                    values.push(stepLog[i].value);
+                    dates.push(stepLog[i].dateTime);
+                }
+            });
+        }
+
+        fetchValues();
+
         this.setState({
             isLoaded: true,
             stepArr: ["8494", "14007", "12567", "3516", "0", "0", "0"],
